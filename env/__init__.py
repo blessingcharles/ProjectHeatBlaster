fc15dc200726b0e5c5d74b029ce1ebfa161d4ad4
th3h04x <mr.th3h04x@gmail.com>
2021-11-04 12:04:25 +0530

    initial commit

--- env/__init__.py
+++
@@ -1,2 +0,0 @@
-# http_config.py ---> config file for request and response handler
-# server_config.py ---> config file for flask server config
--- env/http_config.py
+++
@@ -1,8 +0,0 @@
-#config file for request and response handlers
-
-HTTP_CONFIG = {
-    "allowed_methods":["GET","POST","PUT","DELETE"],
-    "exclude_headers" : ["content-encoding", "content-length", "transfer-encoding", "connection"],
-    "block_bad_useragents":True,
-    "load_balance_algo":"round_robin"
-}
