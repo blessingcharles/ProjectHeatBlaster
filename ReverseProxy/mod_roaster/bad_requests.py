f416461fc0fe87c2bd8473836825d9572b892adf
th3h04x <mr.th3h04x@gmail.com>
2021-12-29 23:27:04 +0530

    payload predictor models added

--- ReverseProxy/mod_roaster/bad_requests.py
+++ ReverseProxy/mod_roaster/bad_requests.py
@@ -1,5 +1,31 @@
 from flask import Request
+import pickle , joblib
+import os
+
+
+from env.ml_config import PAYLOADS_FILE_PATH , PAYLOADS_ML_MODEL
+from env.proxy_config import ROASTING_ML_MODELS 
+
 
 def block_badrequests(request : Request):
     print("[+]----------------------------" , request.args)
-    pass
+    
+
+    try:
+        from ReverseProxy.utils.util import process_ua 
+
+        file_path = PAYLOADS_FILE_PATH + "/" + PAYLOADS_ML_MODEL[ROASTING_ML_MODELS["payloads"]]
+
+        with open(file_path , "rb") as f:
+            model = joblib.load(f)
+        
+        result = model.predict([request.headers["User-Agent"]])
+  
+        print("[+]----------------------------" , request.args)
+
+        return result[0]
+
+    except Exception as e:
+        print(e)
+        return 0
+
--- env/ml_config.py
+++ env/ml_config.py
@@ -13,7 +13,13 @@ UA_ML_MODELS = {
     "RandomForest":"rfc_cv_pipe.pkl"
 }
 
-PAYLOADS_FILE_PATH = f"{pwd}/blasterModels/payload_predictors"
+
+PAYLOADS_FILE_PATH = f"{pwd}/blasterModels/payloads_models"
 PAYLOADS_ML_MODEL = {
-    
+    "LogisticRegression":"lg_cv_pipe.pkl",
+    "SupportVectorClassifier":"svc_cv_pipe.pkl",
+    "KnearestNeighbours":"knn_cv_pipe.pkl",
+    "DecisionTreeClassifier":"dtc_cv_pipe.pkl",
+    "MultinomialNaiveBayes":"mnb_cv_pipe.pkl"  ,
+    "RandomForest":"rfc_cv_pipe.pkl"
 }
